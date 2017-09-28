86bfb3d38a61143c390b28640e1b4f79dfa96557
Karol Marso <karci.marso@gmail.com>
2017-09-28 11:02:16 +0200

    Karol Marso & Mario Kappert Subject: GCD - working on FPGA ...

--- src/gcd.vhd
+++ src/gcd.vhd
@@ -20,7 +20,7 @@ ENTITY gcd IS
           reset: IN std_logic;				   -- Reset the module.
           req:   IN std_logic;				   -- Input operand / Start computation.
           AB:    IN unsigned(15 downto 0);	-- The two operands.
-          ack:   OUT std_logic;				   -- Computation is complete.
+          ack:   OUT std_logic;	
           C:     OUT unsigned(15 downto 0));	-- The result.
 END gcd;
 
@@ -43,47 +43,58 @@ BEGIN
    next_reg_a <= reg_a;
    next_reg_b <= reg_b;
    ack <= '0';
+   C <= reg_b;
    CASE (state) IS
 
 		when start =>
 			if  req = '1' then
 				next_state <= read_A;
+		    else  
+                -- C <= (others=>'0'); --optional
 			end if;
 		when read_A =>
-			next_reg_a <= AB;
-			ack <= '1';
-			next_state <= idle_a;
+		     if req ='1' then
+			     next_reg_a <= AB;
+			     ack <= '1';
+			     next_state <= read_A;
+			  else
+			     next_state <= idle_A;
+			  end if;
 		when idle_A =>
-		    ack <= '1';
 		    if  req = '0' then
-		 	ack <= '0';
-			next_state <= read_b;
+                ack <= '0';
+                next_state <= idle_A;
+			else 
+			    next_state <= read_b;
 		    end if;
 		when read_b =>
-			next_reg_b <= AB;
-               		next_state <= compare;
+		    
+			    next_reg_b <= AB;
+                next_state <= compare;
 			
 		when compare =>
+		     
 			if reg_A = reg_B then
-				next_state <= result;
+			     next_state <= result;
 			else 
 				if reg_A > reg_B then
-					next_state <= A_bigger_than_B;
+				next_state <= A_bigger_than_B;
 				else
-					next_state <= A_smaller_than_B;
+				next_state <= A_smaller_than_B;
 				end if;
 			end if;
 		when A_bigger_than_B =>
-			next_reg_a <= reg_a - reg_b;
-			next_state <= compare;
+                next_reg_a <= reg_a - reg_b;
+                next_state <= compare;
 		when A_smaller_than_B =>
-			next_reg_b <= reg_b - reg_a;
-			next_state <= compare;
+                next_reg_b <= reg_b - reg_a;
+                next_state <= compare;
 		when result =>
-			C <= reg_a;
-			ack <= '1';  
-			next_state <= start;
-
+                C <= reg_a;
+                ack <= '1'; 
+			if req = '0' then 
+                next_state <= start;
+            end if;
    END CASE;
    
 END PROCESS CL;
@@ -92,14 +103,17 @@ END PROCESS CL;
 
 seq: PROCESS (clk, reset)
 BEGIN
-   if reset = '1' then
-		state <= start;
-   elsif rising_edge(clk) then	
+       if reset = '1' then
+            state <= start;
+            reg_a <= (others => '0');
+            reg_b <= (others => '0');
+            
+       elsif rising_edge(clk) then	
 	-- on reset clean registers and get to the starting state	
 	
-	state <= next_state;
-	reg_a <= next_reg_a;
-	reg_b <= next_reg_b;
+            state <= next_state;
+            reg_a <= next_reg_a;
+            reg_b <= next_reg_b;
 
   end if;
 END PROCESS seq;
